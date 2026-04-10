b807992181938348f09a84d8173bfcb4301fe8a3
r86181555-hash <r86181555@gmail.com>
2026-04-10 22:05:01 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,37 +1,29 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
-import { 
-  getAuth, 
-  GoogleAuthProvider, 
-  signInWithRedirect,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } 
+from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBND3n2ag9qGZG5SJPOKNVYr2dHNLwoD7Y",
   authDomain: "rhk-music-24bbc.firebaseapp.com",
-  projectId: "rhk-music-24bbc"
+  projectId: "rhk-music-24bbc",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-const btn = document.getElementById("googleLogin");
+// LOGIN
+window.googleLogin = function () {
+  signInWithPopup(auth, provider)
+    .then(() => {
+      window.location.href = "home.html";
+    })
+    .catch(err => alert(err.message));
+};
 
-// 🔥 CHECK LOCAL LOGIN FIRST
-if (localStorage.getItem("loggedIn") === "true") {
-  window.location.href = "home.html";
-}
-
-// FIREBASE CHECK
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    localStorage.setItem("loggedIn", "true"); // save login
+// AUTO LOGIN
+onAuthStateChanged(auth, user => {
+  if (user && window.location.pathname.includes("index.html")) {
     window.location.href = "home.html";
   }
 });
-
-// LOGIN BUTTON
-btn.onclick = () => {
-  signInWithRedirect(auth, provider);
-};
