ff775f4d2dca9195efe2b1c48def379b24dfa7f8
r86181555-hash <r86181555@gmail.com>
2026-04-10 20:34:36 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup } 
-from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
+import { 
+  getAuth, 
+  GoogleAuthProvider, 
+  signInWithRedirect, 
+  getRedirectResult 
+} from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBND3n2ag9qGZG5SJPOKNVYr2dHNLwoD7Y",
   authDomain: "rhk-music-24bbc.firebaseapp.com",
-  projectId: "rhk-music-24bbc",
-  storageBucket: "rhk-music-24bbc.firebasestorage.app",
-  messagingSenderId: "571438674805",
-  appId: "1:571438674805:web:305cdbf3866febf208193c"
+  projectId: "rhk-music-24bbc"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -16,11 +17,15 @@ const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 document.getElementById("googleLogin").onclick = () => {
-  signInWithPopup(auth, provider)
-    .then(() => {
-      window.location.href = "home.html";
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
+  signInWithRedirect(auth, provider);
 };
+
+getRedirectResult(auth)
+  .then((result) => {
+    if (result) {
+      window.location.href = "home.html";
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
