1a93b9cbf0ca0012b5e9d2bc9c2ef4624837a195
r86181555-hash <r86181555@gmail.com>
2026-04-10 16:10:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const API_KEY = "AIzaSyDdklLjpuYqiQU1akYheP7K3aOLxgQTEtM";
 
+/* TRENDING */
+function loadTrending() {
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=10&q=trending songs india&key=${API_KEY}`)
+    .then(res => res.json())
+    .then(data => {
+
+        let charts = document.getElementById("charts");
+        charts.innerHTML = "";
+
+        data.items.forEach(v => {
+            let id = v.id.videoId;
+            let title = v.snippet.title;
+            let img = v.snippet.thumbnails.medium.url;
+
+            charts.innerHTML += `
+            <div class="card" onclick="play('${id}','${title}','${img}')">
+                <img src="${img}">
+                <p>${title}</p>
+            </div>`;
+        });
+    });
+}
+
+/* SEARCH */
 function searchYT(query) {
     if (query.length < 2) return;
 
@@ -24,13 +48,19 @@ function searchYT(query) {
     });
 }
 
+/* PLAY */
 function play(id,title,img){
     localStorage.setItem("vid", id);
     localStorage.setItem("title", title);
     localStorage.setItem("img", img);
+
+    document.getElementById("miniImg").src = img;
+    document.getElementById("miniTitle").innerText = title;
+
     window.location = "player.html";
 }
 
+/* NAV */
 function goHome(){
     location.reload();
 }
@@ -42,3 +72,10 @@ function focusSearch(){
 function openFav(){
     window.location = "fav.html";
 }
+
+function openPlayerPage(){
+    window.location = "player.html";
+}
+
+/* LOAD */
+window.onload = loadTrending;
