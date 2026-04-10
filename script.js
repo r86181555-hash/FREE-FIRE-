499a9b583b0b9ded1f3d3f043f228ec8666196f1
r86181555-hash <r86181555@gmail.com>
2026-04-10 22:16:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,60 @@
-// 🔥 YOUR YOUTUBE API KEY
 const API_KEY = "AIzaSyDdklLjpuYqiQU1akYheP7K3aOLxgQTEtM";
 
-// 🔍 SEARCH
-async function searchSongs() {
-  let query = document.getElementById("search").value;
+function searchYouTube(query) {
+    let empty = document.getElementById("empty");
+    let container = document.getElementById("results");
 
-  if (!query) {
-    alert("Enter song name");
-    return;
-  }
+    if (query.length < 2) {
+        container.innerHTML = "";
+        empty.style.display = "block";
+        return;
+    }
 
-  let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&maxResults=10&key=${API_KEY}`;
+    empty.style.display = "none";
 
-  let res = await fetch(url);
-  let data = await res.json();
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=10&q=${query}&key=${API_KEY}`)
+    .then(res => res.json())
+    .then(data => {
 
-  displaySongs(data.items);
+        container.innerHTML = "";
+
+        data.items.forEach(video => {
+            let vid = video.id.videoId;
+            let title = video.snippet.title;
+            let img = video.snippet.thumbnails.medium.url;
+
+            container.innerHTML += `
+            <div class="card">
+                <img src="${img}" onclick="openPlayer('${vid}','${title}','${img}')">
+                <p>${title}</p>
+                <button onclick="addFav('${vid}','${title}','${img}')">❤️</button>
+            </div>`;
+        });
+    });
 }
 
-// 🎵 DISPLAY
-function displaySongs(videos) {
-  let container = document.getElementById("results");
-  container.innerHTML = "";
+function openPlayer(id, title, img) {
+    localStorage.setItem("videoId", id);
+    localStorage.setItem("title", title);
+    localStorage.setItem("img", img);
+    window.location.href = "player.html";
+}
 
-  videos.forEach(video => {
-    let vidId = video.id.videoId;
-    let title = video.snippet.title;
-    let thumb = video.snippet.thumbnails.medium.url;
+function addFav(id, title, img) {
+    let fav = JSON.parse(localStorage.getItem("fav")) || [];
+    fav.push({id, title, img});
+    localStorage.setItem("fav", JSON.stringify(fav));
+    alert("Added ❤️");
+}
 
-    let div = document.createElement("div");
-    div.className = "card";
+function openFav() {
+    window.location.href = "fav.html";
+}
 
-    div.innerHTML = `
-      <img src="${thumb}">
-      <p>${title}</p>
-    `;
+function goHome() {
+    window.location.href = "index.html";
+}
 
-    div.onclick = () => {
-      window.location.href = "player.html?id=" + vidId;
-    };
-
-    container.appendChild(div);
-  });
+function focusSearch() {
+    document.getElementById("searchInput").focus();
 }
